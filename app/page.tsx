9126670fca226d4c6955fbf6cd6ed4583b435c2c
gaiusYan <gaiusyanbena11@gmail.com>
2025-05-12 16:12:35 +0200

    delete home directory

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-import HomePage from "@/app/home/page";
+import Navbar from "@/app/mandja-web/_components/navbar";
+import { Communauty } from "@/components/home/communauty";
+import { Culture } from "@/components/home/culture";
+import { HeadLine } from "@/components/home/headline";
+import { MainCarousel } from "@/components/home/main-carousel";
 
 export default function Home() {
   return (
     <>
-      <HomePage/>
+      <Navbar/>
+      <div className="flex flex-col items-center bg-gray-50 h-full">
+        <MainCarousel />
+        <Culture />
+        <HeadLine/>
+        <Communauty/>
+      </div>
     </>    
   );
 }
